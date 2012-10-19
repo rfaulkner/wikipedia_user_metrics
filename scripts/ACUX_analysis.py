9cfba5cb8d57acc439a6d33a568b93b6b6521546
rfaulk <rfaulkner@wikimedia.org>
2012-10-19 16:11:26 -0700

    added logic to make selecting log files more flexible.  Args can now be passed in place of manually modifying the script.

--- scripts/ACUX_analysis.py
+++ scripts/ACUX_analysis.py
@@ -29,8 +29,11 @@ sys.path.append(settings.__project_home__)
 
 import logging
 import argparse
+import os
+import re
 import src.etl.data_loader as dl
-import src.etl.experiments_loader as el
+import datetime
+from dateutil.parser import parse as date_parse
 
 # CONFIGURE THE LOGGER
 logging.basicConfig(level=logging.INFO, stream=sys.stderr, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%b-%d %H:%M:%S')
@@ -41,32 +44,38 @@ def main(args):
 
     # initialize dataloader objects
     global data_loader
-    data_loader = dl.DataLoader(db='slave')
+    data_loader = dl.Handle(db='slave')
 
-    global exp_loader
-    exp_loader = el.ExperimentsLoader()
+    if not isinstance(args.start_date, datetime.datetime):
+        start_date = date_parse(args.start_date)
+    else:
+        start_date = args.start_date
+
+    if not isinstance(args.end_date, datetime.datetime):
+        end_date = date_parse(args.end_date)
+    else:
+        end_date = args.end_date
 
     if args.load:
-        load_log_data()
+        logs = get_log_files(start_date, end_date)
+        load_log_data(logs)
 
     if args.clickthrough:
         get_click_through()
 
 
 def read_file(filepath_name):
-
-    sql_file_obj = open(filepath_name,'r')
-
+    file_obj = open(filepath_name,'r')
     sql = ''
-    line = sql_file_obj.readline()
-    while (line):
+    line = file_obj.readline()
+    while line:
         sql += line + ' '
-        line = sql_file_obj.readline()
-    sql_file_obj.close()
+        line = file_obj.readline()
+    file_obj.close()
 
     return sql
 
-def load_log_data():
+def load_log_data(logs):
 
     global data_loader
 
@@ -81,18 +90,34 @@ def load_log_data():
 
     lpm = dl.DataLoader.LineParseMethods()
 
-    # Load users from logs
-
-    logs = ['clicktracking.log-20121004.gz','clicktracking.log-20121005.gz','clicktracking.log-20121006.gz',
-            'clicktracking.log-20121007.gz','clicktracking.log-20121008.gz','clicktracking.log-20121009.gz',
-            'clicktracking.log-20121010.gz',]
-
     for log in logs:
+        logging.info('Loading clicktracking log: %s' % log)
         data_loader.create_table_from_xsv(log,'','e3_acux1_client_events',
-            parse_function=lpm.e3_acux_log_parse, regex_list=['ext.accountCreationUX'], header=False)
+            parse_function=lpm.e3_acux_log_parse, regex_list=['ext.accountCreationUX'], header=False, max_records=100000)
         data_loader.create_table_from_xsv(log,'','e3_acux1_server_events',
-            parse_function=lpm.e3_acux_log_parse, regex_list=['event_id=account_create'], header=False)
+            parse_function=lpm.e3_acux_log_parse, regex_list=['event_id=account_create'], header=False, max_records=100000)
 
+
+def get_log_files(start_date, end_date):
+
+    dir_listing = os.listdir(settings.__data_home__)
+    dir_listing.sort()
+    i = dir_listing.__iter__()
+
+    prefix = "clicktracking.log-"
+    curr_date = start_date
+    logs = list()
+
+    try:
+        while curr_date <= end_date:
+            fname = i.next()
+            file_pattern = prefix + curr_date.strftime('%Y%m%d')
+            while not re.search(file_pattern, fname): fname = i.next()
+            logs.append(fname)
+            curr_date += datetime.timedelta(days=1)
+    except StopIteration:
+        pass
+    return logs
 
 def get_click_through():
 
@@ -165,14 +190,9 @@ def get_click_through():
     data_loader.execute_SQL('drop table if exists e3_acux1_created')
     data_loader._cur_.execute(" ".join(sql.strip().split()))
 
-def get_blocks(experiment_start_date):
-    return
-
-def get_bytes_added(eligible_users, experiment_start_date):
-    return
-
-def get_time_to_threshold(eligible_users):
-    return
+def get_blocks(experiment_start_date): pass
+def get_bytes_added(eligible_users, experiment_start_date): pass
+def get_time_to_threshold(eligible_users): pass
 
 # Call Main
 if __name__ == "__main__":
@@ -182,8 +202,14 @@ if __name__ == "__main__":
         epilog="",
         conflict_handler="resolve"
     )
+
+    default_max_time = datetime.datetime.now()
+    default_min_time = default_max_time + datetime.timedelta(days=-1)
+
     parser.add_argument('-l', '--load',type=bool, help='Load the clicktracking logs.',default=False)
     parser.add_argument('-c', '--clickthrough',type=bool, help='Compute event click through.',default=False)
+    parser.add_argument('-s', '--start_date',type=str, help='Start date of logs to load.',default=str(default_min_time))
+    parser.add_argument('-e', '--end_date',type=str, help='Compute event click through.',default=str(default_max_time))
 
     args = parser.parse_args()
 
