de72e86bb568bd4580da6f9be32bff4b278bc7a7
rfaulk <rfaulkner@wikimedia.org>
2012-10-24 18:30:26 -0700

    fixed several issues to conform script with latest metrics definitions

--- scripts/PEF2_analysis.py
+++ scripts/PEF2_analysis.py
@@ -22,6 +22,7 @@ __date__ = "October 3rd, 2012"
 __license__ = "GPL (version 2 or later)"
 
 import sys
+import os
 import settings
 sys.path.append(settings.__project_home__)
 
@@ -38,20 +39,40 @@ from dateutil.parser import *
 logging.basicConfig(level=logging.INFO, stream=sys.stderr, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%b-%d %H:%M:%S')
 
 def main(args):
+    """
+        Data gathering for Post-Edit Feedback iteration #2
+            - it is assumed that `staging`.`e3_pef_iter2_bucketed_users` exists
+    """
 
     # initialize dataloader objects
     global data_loader
     data_loader = dl.Handle(db='slave')
 
+    global experiment_start_date
+    experiment_start_date = datetime.datetime(year=2012,month=9,day=20)
+
     # Load eligible users
+    logging.info('Retrieving namespace edits...')
     sql = 'select user_id from staging.e3_pef_iter2_bucketed_users'
     eligible_users = data_loader.cast_elems_to_string(data_loader.get_elem_from_nested_list(data_loader.execute_SQL(sql),0))
     user_id_str = data_loader.format_comma_separated_list(eligible_users)
+    logging.info('There are %s eligible users.  Processing ...' % len(eligible_users))
+
+    # PROCESS BLOCKS
+    if args.blocks_table:
+        get_blocks()
+        # create index user_id_idx on e3_pef_iter2_blocks (user_id);
+
+    if args.global_user_diff_table:
+        data_loader.execute_SQL('drop table if exists e3_global_user_diff')
+        sql = dl.read_file(settings.__sql_home__ + 'create_e3_global_user_diff.sql')
+        data_loader.execute_SQL(sql)
+        # create index user_id_idx on e3_global_user_diff (user_id);
 
     # Create a table storing edits by namespace
     if args.ns_table:
+        logging.info('Retrieving namespace edits...')
         data_loader.execute_SQL('drop table if exists e3_pef_iter2_ns')
-
         sql = """
                 create table e3_pef_iter2_ns
                     select
@@ -65,53 +86,46 @@ def main(args):
                     group by 1,2
                     order by 1 asc, 2 asc
             """ % { 'users' : user_id_str }
-
         data_loader.execute_SQL(" ".join(sql.strip().split()))
 
     # Get eligible users
-    eligible_users = data_loader.cast_elems_to_string(data_loader.get_elem_from_nested_list(data_loader.execute_SQL('select distinct rev_user from e3_pef_iter2_ns'),0))
-    logging.info('There are %s eligible user.  Processing ...' % len(eligible_users))
-    experiment_start_date = datetime.datetime(year=2012,month=9,day=20)
+    if args.filter:
+        logging.info('Retrieving eligible users filtered by blocks and global/local user registration...')
+        sql = """
+                select
+                distinct rev_user
+                from e3_pef_iter2_ns as ns
+                    join e3_pef_iter2_blocks as b
+                        on ns.rev_user = b.user_id
+                    join e3_global_user_diff as gu
+                        on gu.user_id = b.user_id
+                where block_count = 0 and delta >= -7
+                """
+        eligible_users = data_loader.cast_elems_to_string(data_loader.get_elem_from_nested_list(data_loader.execute_SQL(sql),0))
+        logging.info('There are %s eligible users.  Processing ...' % len(eligible_users))
 
     # PROCESS TIME TO THRESHOLD
     if args.bytes_added_table:
-        get_bytes_added(eligible_users, experiment_start_date)
+        get_bytes_added(eligible_users)
 
     # PROCESS TIME TO THRESHOLD
     if args.time_to_threshold_table:
         get_time_to_threshold(eligible_users)
+        get_time_to_threshold(eligible_users,outfile='e3_pef_iter2_survival_bucket.tsv',threshold_edit=ttt.LAST_EDIT)
 
-    # PROCESS BLOCKS
-    if args.blocks_table:
-        get_blocks(experiment_start_date)
 
-    if args.gloabal_user_diff_table:
-        sql = read_file(settings.__sql_home__ + 'create_e3_global_user_diff.sql')
-        data_loader.execute_SQL(sql)
 
-def read_file(filepath_name):
-
-    sql_file_obj = open(filepath_name,'r')
-
-    sql = ''
-    line = sql_file_obj.readline()
-    while line:
-        sql += line + ' '
-        line = sql_file_obj.readline()
-    sql_file_obj.close()
-
-    return sql
-
-def get_blocks(experiment_start_date):
+def get_blocks():
     """ Process blocks """
-
+    global experiment_start_date
+    logging.info("Processing blocked users for %s" % str(experiment_start_date))
     sql = """
             select
                 r.user_id,
                 user.user_name
             from (  select
-                        distinct rev_user as user_id
-                    from e3_pef_iter2_ns) as r
+                        distinct user_id as user_id
+                    from e3_pef_iter2_bucketed_users) as r
             join enwiki.user on r.user_id = user.user_id
           """
     results = data_loader.execute_SQL(sql)
@@ -126,7 +140,6 @@ def get_blocks(experiment_start_date):
             user_handle = user_handle.replace(" ", "_")
         h[user_handle] = row[0]
 
-    logging.info("Processing blocked users for %s" % str(experiment_start_date))
     eligible_user_names = data_loader.cast_elems_to_string(data_loader.get_elem_from_nested_list(results,1))
     block_list = b.Blocks(date_start=str(experiment_start_date)).process(eligible_user_names)._results
 
@@ -139,36 +152,33 @@ def get_blocks(experiment_start_date):
             pass
 
     data_loader.list_to_xsv(block_list)
-    sql = read_file(settings.__sql_home__ + 'create_e3_pef_iter2_blocks.sql')
+    data_loader.execute_SQL('drop table if exists e3_pef_iter2_blocks')
+    sql = dl.read_file(settings.__sql_home__ + 'create_e3_pef_iter2_blocks.sql')
     data_loader.create_table_from_xsv('list_to_xsv.out', sql, 'e3_pef_iter2_blocks')
+    os.system('cp %(data_dir)slist_to_xsv.out %(data_dir)se3_pef_iter2_blocks.tsv' % {'data_dir' : settings.__data_home__})
 
-def get_bytes_added(eligible_users, experiment_start_date):
+def get_bytes_added(eligible_users):
     """ process "bytes added" """
+    global experiment_start_date
 
+    logging.info("Processing bytes added for %s users." % str(len(eligible_users)))
     sql_reg_date = 'select user_registration from enwiki.user where user_id = %s;'
 
     bytes_added = list()
     bad_users = 0
-    count = 0
+    logging.info('Processing %s eligible users...' % str(len(eligible_users)))
+
     for user in eligible_users:
-
-        if count % 500 == 0:
-            logging.info('Processed %s eligible users...' % count)
-
         try:
-
             reg_date = parse(data_loader.execute_SQL(sql_reg_date % user)[0][0])
             end_date = reg_date + datetime.timedelta(days=14)
-
             entry = ba.BytesAdded(date_start=reg_date, date_end=end_date).process([user]).__iter__().next()
-            entry.append(str((reg_date-experiment_start_date).seconds / 3600))
+            # entry.append(str((reg_date-experiment_start_date).seconds / 3600))
             bytes_added.append(entry)
 
         except Exception as e:
-            logging.error('Could not get bytes added for user %s: %s' % (str(user), str(e)))
+            logging.error('Could not get bytes added for user %s: %s' % (str(user), e.message))
             bad_users += 1
-
-        count += 1
 
     logging.info('Missed %s users out of %s.' % (str(bad_users), str(len(eligible_users))))
     logging.info('Writing results to table.')
@@ -177,7 +187,7 @@ def get_bytes_added(eligible_users, experiment_start_date):
 
     # Create table
     data_loader.execute_SQL('drop table if exists e3_pef_iter2_bytesadded')
-    sql = read_file(settings.__sql_home__ + 'create_e3_pef_iter2_bytes_added.sql')
+    sql = dl.read_file(settings.__sql_home__ + 'create_e3_pef_iter2_bytes_added.sql')
     data_loader.create_table_from_xsv('list_to_xsv.out', sql, 'e3_pef_iter2_bytesadded')
 
     sql = """
@@ -194,20 +204,19 @@ def get_bytes_added(eligible_users, experiment_start_date):
                     join e3_pef_iter2_bucketed_users as d
                     on d.user_id = r.user_id
             """
-
     data_loader.create_xsv_from_SQL(" ".join(sql.strip().split()), outfile = 'e3_pef_iter2_ba_bucket.tsv')
 
-def get_time_to_threshold(eligible_users):
-    """
-        Time to Threshold metric processing
-    """
+def get_time_to_threshold(eligible_users, first_edit=1, threshold_edit=2,outfile='e3_pef_iter2_ttt_bucket.tsv'):
+    """ Time to Threshold metric processing """
+    logging.info("Processing blocked users for %s users." % str(len(eligible_users)))
 
     # create table
-    sql = read_file(settings.__sql_home__ + 'create_e3_pef_iter2_timetothreshold.sql')
-
+    sql = dl.read_file(settings.__sql_home__ + 'create_e3_pef_iter2_timetothreshold.sql')
     t = ttt.TimeToThreshold(ttt.TimeToThreshold.EditCountThreshold,
-        first_edit=1, threshold_edit=2).process(eligible_users)
+        first_edit=first_edit, threshold_edit=threshold_edit).process(eligible_users)
+
     data_loader.list_to_xsv(t)
+    data_loader.execute_SQL('drop table if exists e3_pef_iter2_timetothreshold')
     data_loader.create_table_from_xsv('list_to_xsv.out', sql, 'e3_pef_iter2_timetothreshold')
 
     sql = """
@@ -218,26 +227,29 @@ def get_time_to_threshold(eligible_users):
             from e3_pef_iter2_timetothreshold as r
                 join e3_pef_iter2_bucketed_users as d on d.user_id = r.user_id
         """
-    data_loader.create_xsv_from_SQL(" ".join(sql.strip().split()),outfile = 'e3_pef_iter1_ttt_bucket.tsv')
-
-    return t
+    data_loader.create_xsv_from_SQL(" ".join(sql.strip().split()),outfile=outfile)
 
 # Call Main
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(
-        description="",
-        epilog="python run.py",
+        description="Script for generating metrics from Post-Edit Feedback iteration 2 Experiment.  " \
+                    "Relies on a connection to an enwiki database.  Expects the existence of a " \
+                    "table containing bucketed users from the experiment.",
+        epilog="",
         conflict_handler="resolve"
     )
-
-    parser.add_argument('-n', '--ns_table',type=bool, help='',default=False)
-    parser.add_argument('-b', '--blocks_table',type=bool, help='',default=False)
-    parser.add_argument('-y', '--bytes_added_table',type=bool, help='',default=False)
-    parser.add_argument('-t', '--time_to_threshold_table',type=bool, help='',default=False)
-    parser.add_argument('-g', '--gloabal_user_diff_table',type=bool, help='',default=True)
-
+    parser.add_argument('-f', '--filter',action="store_true",help='Filter eligible users.',default=False)
+    parser.add_argument('-n', '--ns_table',action="store_true",help='Process namespace edits.',default=False)
+    parser.add_argument('-b', '--blocks_table',action="store_true",help='Process blocks.',default=False)
+    parser.add_argument('-y', '--bytes_added_table',action="store_true",help='Process bytes added metric',default=False)
+    parser.add_argument('-t', '--time_to_threshold_table',action="store_true",
+        help='process time to threshold metric.',
+        default=False)
+    parser.add_argument('-g', '--global_user_diff_table',action="store_true",
+        help='Process globaluser timestamp filter.',
+        default=False)
     args = parser.parse_args()
     logging.info('Arguments: %s' % args)
-
     sys.exit(main(args))
+
+
