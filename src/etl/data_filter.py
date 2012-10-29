6c0ac588095f78d030bb1db59f810c60facf9f1a
rfaulk <rfaulkner@wikimedia.org>
2012-10-29 13:32:34 -0700

    rewrote filter bots to use halfak.bot table

--- src/etl/data_filter.py
+++ src/etl/data_filter.py
@@ -8,15 +8,15 @@ __license__ = "GPL (version 2 or later)"
 
 import sys
 import logging
-import re
+import heapq
 import data_loader as dl
 
 # CONFIGURE THE LOGGER
 logging.basicConfig(level=logging.DEBUG, stream=sys.stderr, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%b-%d %H:%M:%S')
 
-def filter_bots(user_ids, project='enwiki'):
+def filter_bots(eligible_users):
     """
-        Filter bots from a user list.
+        Filter bots based on bot table.  TODO: This can be sped up by sorting lists first.
 
         Parameters:
             - **users** - list of user names to be matched on the bot REGEX
@@ -24,34 +24,22 @@ def filter_bots(user_ids, project='enwiki'):
         Return:
             - List(string).  Filtered results.
     """
-
-    BOT_REGEX = r'[Bb][Oo][Tt]'
-
-    # Extract user names from the db
-    d = dl.Handle(db='slave')
-    user_id_str = d.format_comma_separated_list(user_ids, include_quotes=False)
-    d._cur_.execute('select user_id, user_name from %(project)s.user where user_id in (%(user_list)s)' % {
-        'project' : project,
-        'user_list' : user_id_str})
-
-    # Select user ids from the input not corresponding to bots
-    return filter(lambda x: not x == -1,
-        map(lambda x: str(x[0]) if not(re.search(BOT_REGEX, str(x[1]))) else -1, d._cur_))
+    BOT_TABLE = 'halfak.bot'
+    d = dl.DataLoader(instance='slave-2')
+    results = [str(x[0]) for x in d.connection.execute_SQL('select user_id from %(table)s' % { 'table' : BOT_TABLE})]
+    return filter(lambda x: str(x) not in results, eligible_users)
 
 class DataFilter(object):
     """ BASE CLASS for filters.  The interface defines the filter method which is called. """
 
     def __init__(self, **kwargs):
         """ Perform initialization boilerplate for all classes """
-
         # The mutable object will contain the data structures on which the filter will operate
         for key in kwargs:
             if key == 'mutable_obj':
                 self._mutable_obj_ = kwargs[key]
 
     def execute(self, **kwargs):
-        """
-            Execution method.  The base class simply performs the logging.
-        """
+        """ Execution method.  The base class simply performs the logging. """
 
         return
