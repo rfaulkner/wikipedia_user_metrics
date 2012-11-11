c50f0c5fbc5106399834d31260855239689aaf11
rfaulk <rfaulkner@wikimedia.org>
2012-11-10 17:58:21 -0800

    added functionality to handle processing blocks, edit volume, and milestone metrics

--- scripts/e3_data_wrangle.py
+++ scripts/e3_data_wrangle.py
@@ -18,8 +18,13 @@ sys.path.append(s.__E3_Analysis_Home__)
 
 import logging
 import argparse
+import datetime
+from dateutil.parser import parse as date_parse
 import src.etl.data_loader as dl
 import e3_experiment_definitions as e3_def
+import src.metrics.bytes_added as ba
+import src.metrics.blocks as b
+import src.metrics.time_to_threshold as ttt
 
 # CONFIGURE THE LOGGER
 logging.basicConfig(level=logging.DEBUG, stream=sys.stderr,
@@ -46,21 +51,114 @@ def load_logs():
         dl.DataLoader().create_table_from_xsv(f, '', exp_meta_data['user_bucket']['table_name'],
             parse_function=lpm, header=False)
 
-def main(args):
+def blocks(users):
+    global exp_meta_data
+    global conn
+
+    logging.info("Processing blocked users for %s" % str(exp_meta_data['start_date']))
+    sql = """
+                select
+                    user_id,
+                    user_name
+                from enwiki.user
+                where user_id in (%(users)s)
+              """ % {'users' : dl.DataLoader().format_comma_separated_list(users, include_quotes=False)}
+    results = conn.execute_SQL(" ".join(sql.strip().split('\n')))
+
+    # dump results to hash
+    h = dict()
+    for row in results:
+        user_handle = row[1]
+        try:
+            user_handle = user_handle.encode('utf-8').replace(" ", "_")
+        except UnicodeDecodeError:
+            user_handle = user_handle.replace(" ", "_")
+        h[user_handle] = row[0]
+
+    eligible_user_names = dl.DataLoader().cast_elems_to_string(dl.DataLoader().get_elem_from_nested_list(results,1))
+    block_list = b.Blocks(date_start=str(exp_meta_data['start_date'])).process(eligible_user_names)._results
+
+    # Replace user names with IDs
+    for i in xrange(len(block_list)):
+        try:
+            block_list[i][0] = str(h[block_list[i][0]])
+        except KeyError:
+            logging.error('Cannot include %s in result.' % block_list[i][0])
+            pass
+
+    dl.DataLoader().list_to_xsv(block_list)  # !! this is inefficient, refactor
+    conn.execute_SQL('drop table if exists %s' % exp_meta_data['blocks']['table_name'])
+    create_sql = " ".join(exp_meta_data['blocks']['definition'].strip().split('\n'))
+    dl.DataLoader().create_table_from_xsv('list_to_xsv.out',create_sql,
+        exp_meta_data['blocks']['table_name'], header=False)
+
+def edit_volume(users, period=1):
+    """ Extracts bytes added and edit count "period" days after registration """
     global exp_meta_data
 
+    exp_start = date_parse(exp_meta_data['start_date'])
+    logging.info("Processing bytes added for %s users." % str(len(users)))
+    sql_reg_date = 'select user_registration from enwiki.user where user_id = %s;'
+
+    bytes_added = list()
+    bad_users = 0
+    logging.info('Processing %s eligible users...' % str(len(users)))
+
+    for user in users:
+        try:
+            reg_date = date_parse(conn.execute_SQL(sql_reg_date % user)[0][0])
+            if reg_date < exp_start: continue
+            end_date = reg_date + datetime.timedelta(days=period)
+            entry = ba.BytesAdded(date_start=reg_date, date_end=end_date).process(user, num_threads=2).__iter__().next()
+            bytes_added.append(entry)
+
+        except Exception as e:
+            logging.error('Could not get bytes added for user %s: %s' % (str(user), e.message))
+            bad_users += 1
+
+    logging.info('Missed %s users out of %s.' % (str(bad_users), str(len(users))))
+    logging.info('Writing results to table.')
+    dl.DataLoader().list_to_xsv(bytes_added)
+
+    # Create table
+    conn.execute_SQL('drop table if exists %s' % exp_meta_data['edit_volume']['table_name'])
+    sql = " ".join(exp_meta_data['edit_volume']['definition'].strip().split('\n'))
+    dl.DataLoader().create_table_from_xsv('list_to_xsv.out', sql,
+        exp_meta_data['edit_volume']['table_name'], header=False)
+
+def time_to_threshold(users, first_edit=1, threshold_edit=2):
+    global exp_meta_data
+
+    logging.info("Processing time to threshold for %s users." % str(len(users)))
+
+    # create table
+    t = ttt.TimeToThreshold(ttt.TimeToThreshold.EditCountThreshold,
+        first_edit=first_edit, threshold_edit=threshold_edit).process(users).__iter__()
+
+    sql = " ".join(exp_meta_data['time_to_milestone']['definition'].strip().split('\n'))
+    dl.DataLoader().list_to_xsv(t)
+    conn.execute_SQL('drop table if exists %s' %  exp_meta_data['time_to_milestone']['table_name'])
+    dl.DataLoader().create_table_from_xsv('list_to_xsv.out', sql,
+        exp_meta_data['time_to_milestone']['table_name'], header=False)
+
+def main(args):
+    global exp_meta_data
     logging.info(args)
 
-    # Load the experiment meta data
-    try:
+    try: # Load the experiment meta data
         exp_meta_data = e3_def.experiments[args.experiment]
     except KeyError:
         logging.error('Experiment not found: %s'  % str(args.experiment))
         return
 
-    if args.load_logs:
-        load_logs()
+    # Process data
+    if args.load_logs: load_logs()
+    sql = exp_meta_data['user_list_sql']
+    users = [str(row[0]) for row in conn.execute_SQL(sql)]
 
+    if args.blocks: blocks(users)
+    if args.edit_volume: edit_volume(users, period=2)
+    if args.time_to_threshold: time_to_threshold(users)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -71,6 +169,10 @@ if __name__ == '__main__':
     )
     parser.add_argument('-e', '--experiment',type=str, help='Experiment.',default='CTA4')
     parser.add_argument('-l', '--load_logs',action="store_true",help='Process log data.',default=False)
+    parser.add_argument('-b', '--blocks',action="store_true",help='.',default=False)
+    parser.add_argument('-e', '--edit_volume',action="store_true",help='.',default=False)
+    parser.add_argument('-t', '--time_to_threshold',action="store_true",help='.',default=False)
+    parser.add_argument('-r', '--revert_data',action="store_true",help='.',default=False)
 
     args = parser.parse_args()
     sys.exit(main(args))
--- scripts/e3_experiment_definitions.py
+++ scripts/e3_experiment_definitions.py
@@ -16,6 +16,8 @@ experiments = {
                                'clicktracking.log-20121105.gz', 'clicktracking.log-20121106.gz',
                                'clicktracking.log-20121107.gz'],
 
+                'start_date' : '20121026000000',
+                'end_date' : '20121107000000',
                 'log_parser_method' : lp.LineParseMethods.e3_cta4_log_parse,
 
                 'user_bucket' : { 'definition' : """
@@ -33,8 +35,42 @@ experiments = {
                                     'table_name' : 'e3_cta4_users'
                                 },
 
-                'bytes_added' : { 'definition' : '',
-                                  'table_name' : ''
-                }
+                'blocks' : { 'definition' : """
+                                            create table `e3_cta4_blocks` (
+                                            `user_id` int(5) unsigned NOT NULL DEFAULT 0,
+                                              `block_count` varbinary(255) NOT NULL DEFAULT '',
+                                              `first_block` varbinary(255) NOT NULL DEFAULT '',
+                                              `last_block` varbinary(255) NOT NULL DEFAULT '',
+                                              `ban` varbinary(255) NOT NULL DEFAULT ''
+                                            ) ENGINE=MyISAM DEFAULT CHARSET=binary
+                                                """,
+                                  'table_name' : 'e3_cta4_blocks'
+                },
+
+                'edit_volume' : { 'definition' : """
+                                                CREATE TABLE `e3_cta4_edit_volume` (
+                                              `user_id` int(5) unsigned NOT NULL DEFAULT 0,
+                                              `bytes_added_net` varbinary(255) NOT NULL DEFAULT '',
+                                              `bytes_added_abs` varbinary(255) NOT NULL DEFAULT '',
+                                              `bytes_added_pos` varbinary(255) NOT NULL DEFAULT '',
+                                              `bytes_added_neg` varbinary(255) NOT NULL DEFAULT '',
+                                              `edit_count` varbinary(255) NOT NULL DEFAULT ''
+                                            ) ENGINE=MyISAM DEFAULT CHARSET=binary
+                                                """,
+                                  'table_name' : 'e3_cta4_edit_volume'
+                },
+
+                'time_to_milestone' : { 'definition' : """
+                                                        CREATE TABLE `e3_cta4_time_to_milestone` (
+                                                          `user_id` int(5) unsigned NOT NULL DEFAULT 0,
+                                                          `time_minutes` varbinary(255) NOT NULL DEFAULT ''
+                                                        ) ENGINE=MyISAM DEFAULT CHARSET=binary
+                                                """,
+                                  'table_name' : 'e3_cta4_time_to_milestone'
+                },
+
+                'user_list_sql' : "select distinct rev_user from e3_cta4_users as e "
+                                  "join enwiki.revision as r on e.add_field_3 = r.rev_id and rev_user > 0"
+
     }
 }
