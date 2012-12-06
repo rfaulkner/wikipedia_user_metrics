2ec104bb4064590989424eac3872b2e22c6dc300
rfaulk <rfaulkner@wikimedia.org>
2012-12-05 18:34:10 -0800

    MOD: updated documentation

--- src/metrics/revert_rate.py
+++ src/metrics/revert_rate.py
@@ -8,7 +8,31 @@ import user_metric as um
 import datetime
 
 class RevertRate(um.UserMetric):
-    """ Skeleton class for "RevertRate" metric:  `https://meta.wikimedia.org/wiki/Research:Metrics/revert_rate` """
+    """
+        Skeleton class for "RevertRate" metric:
+
+            `https://meta.wikimedia.org/wiki/Research:Metrics/revert_rate`
+
+        This metric computes how often a user has been reverted
+
+        As a UserMetric type this class utilizes the process() function attribute to produce an internal list of metrics by
+        user handle (typically ID but user names may also be specified). The execution of process() produces a nested list that
+        stores in each element:
+
+            * User ID
+            * Revert rate over the period of measurement
+            * Total revisions over the period of measurement
+
+        For example to produce the above datapoint for a user id one could call: ::
+
+            >>> import src.metrics.revert_rate as rr
+            >>> rr.RevertRate(date_start='2012-07-30 00:00:00',look_back=10,look_ahead=10).process([13234584]).__iter__().next()
+            [13234584, 0.0, 2.0]
+
+        In this call `look_ahead` and `look_back` indicate how many revisions in the past and in the future for a given
+        article we are willing to look for a revert.  The identification of reverts is done by matching sha1 checksum values
+        over revision history.
+    """
 
     REV_SHA1_IDX = 2
     REV_USER_TEXT_IDX = 1
@@ -68,6 +92,7 @@ class RevertRate(um.UserMetric):
         return self
 
     def __revert(self, rev_id, page_id, sha1, user_text):
+        """ Returns the revision corresponding to a revision if it exists. """
         history = {}
         for rev in self.__history(self._data_source_._db_, rev_id, page_id, self.look_back, project=self._project_):
             history[rev[self.REV_SHA1_IDX]] = rev
@@ -81,15 +106,16 @@ class RevertRate(um.UserMetric):
 
     @staticmethod
     def __history(conn, rev_id, page_id, n, project='enwiki'):
+        """ Produce the n revisions on a page before a given revision """
         cursor = conn.cursor()
         cursor.execute(
             """
-            SELECT rev_id, rev_user_text, rev_sha1
-            FROM %(project)s.revision
-            WHERE rev_page = %(page_id)s
-            AND rev_id < %(rev_id)s
-            ORDER BY rev_id DESC
-            LIMIT %(n)s
+                SELECT rev_id, rev_user_text, rev_sha1
+                FROM %(project)s.revision
+                WHERE rev_page = %(page_id)s
+                    AND rev_id < %(rev_id)s
+                ORDER BY rev_id DESC
+                LIMIT %(n)s
             """ % {
                 'rev_id':  rev_id,
                 'page_id': page_id,
@@ -103,15 +129,16 @@ class RevertRate(um.UserMetric):
 
     @staticmethod
     def __future(conn, rev_id, page_id, n, project='enwiki'):
+        """ Produce the n revisions on a page after a given revision """
         cursor = conn.cursor()
         cursor.execute(
             """
-            SELECT rev_id, rev_user_text, rev_sha1
-            FROM %(project)s.revision
-            WHERE rev_page = %(page_id)s
-            AND rev_id > %(rev_id)s
-            ORDER BY rev_id ASC
-            LIMIT %(n)s
+                SELECT rev_id, rev_user_text, rev_sha1
+                FROM %(project)s.revision
+                WHERE rev_page = %(page_id)s
+                    AND rev_id > %(rev_id)s
+                ORDER BY rev_id ASC
+                LIMIT %(n)s
             """ % {
                 'rev_id':  rev_id,
                 'page_id': page_id,
