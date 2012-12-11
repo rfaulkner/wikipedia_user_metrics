b642f6e1eb4cf6048c9e4f03ee2fe3bac111490f
rfaulk <rfaulkner@wikimedia.org>
2012-12-10 18:43:01 -0800

    ADD: allow list of user_ids to be passed for time series generation

--- scripts/generate_time_series.py
+++ scripts/generate_time_series.py
@@ -31,9 +31,16 @@ def main(args):
         logging.error('Specify valid resolution int. 0=hour,1=day,2=week,3=month.')
         return
 
+    # if the argument specifying SQL for user IDs has been passed generate uids
+    uids=None
+    if args.user_sql:
+        conn = dl.Connector(instance='slave')
+        conn._cur_.execute(args.user_sql)
+        uids = [r[0] for r in conn._cur_][:10]
+
     # Write to a tsv
     try:
-        dl.DataLoader().list_to_xsv(tspm.DataTypeMethods.DATA_TYPE[args.data_type](args, interval))
+        dl.DataLoader().list_to_xsv(tspm.DataTypeMethods.DATA_TYPE[args.data_type](args, interval, user_ids=uids))
     except KeyError:
         logging.info('Invalid data type.')
 
@@ -45,7 +52,6 @@ if __name__ == "__main__":
 
     today = "".join([today.strftime('%Y%m%d'), "000000"])
     yesterday = "".join([yesterday.strftime('%Y%m%d'),"000000"])
-    out_filename = 'dau_out_' + yesterday[:8] + '-' + today[:8] + '.tsv'
 
     parser = argparse.ArgumentParser(
         description="This script computes generates a tsv conataning rev counts for dailiy active users.",
@@ -59,6 +65,7 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--date_start',type=str, help='Start date of measurement.', default=yesterday)
     parser.add_argument('-e', '--date_end',type=str, help='End date of measurement.', default=today)
     parser.add_argument('-t', '--data_type',type=str, help='Type of data to gather.', default='prod')
+    parser.add_argument('-u', '--user_sql',type=str, help='Specify user SQL query to get IDs.', default='')
     args = parser.parse_args()
 
     sys.exit(main(args))
--- src/etl/time_series_process_methods.py
+++ src/etl/time_series_process_methods.py
@@ -11,8 +11,10 @@ import datetime
 from dateutil.parser import parse as date_parse
 import src.metrics.threshold as th
 
-def productive_editors_by_day(args, interval,log=True):
+def productive_editors_by_day(args, interval,log=True, user_ids=None):
     """ Computes a list of threshold metrics """
+
+    if user_ids is None: user_ids = list() # assign an empty list in case default arg is used
 
     c = date_parse(args.date_start)
     e = date_parse(args.date_end)
@@ -29,7 +31,7 @@ def productive_editors_by_day(args, interval,log=True):
         if log:
             print str(datetime.datetime.now()) + ' - Processing time series data for %s...' % str(ts_list[i])
 
-        for r in th.Threshold(date_start=ts_list[i], date_end=ts_list[i+1],n=1,t=1440).process([]).__iter__():
+        for r in th.Threshold(date_start=ts_list[i], date_end=ts_list[i+1],n=1,t=1440).process(user_ids).__iter__():
             try:
                 if r[1]: pos+=1
             except IndexError: continue
