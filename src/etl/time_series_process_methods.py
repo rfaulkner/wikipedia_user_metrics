0fd668226e00ddb5b98dfa35aebb285233e7c4a8
rfaulk <rfaulkner@wikimedia.org>
2012-12-10 17:01:16 -0800

    ADD: more verbose logging + flag to toggle logging

--- src/etl/time_series_process_methods.py
+++ src/etl/time_series_process_methods.py
@@ -11,7 +11,7 @@ import datetime
 from dateutil.parser import parse as date_parse
 import src.metrics.threshold as th
 
-def productive_editors_by_day(args, interval):
+def productive_editors_by_day(args, interval,log=True):
     """ Computes a list of threshold metrics """
 
     c = date_parse(args.date_start)
@@ -25,13 +25,21 @@ def productive_editors_by_day(args, interval):
     for i in xrange(len(ts_list) - 1):
         total=0
         pos=0
-        print str(datetime.datetime.now()) + ' - Processing time series data for %s...' % str(ts_list[i])
+
+        if log:
+            print str(datetime.datetime.now()) + ' - Processing time series data for %s...' % str(ts_list[i])
+
         for r in th.Threshold(date_start=ts_list[i], date_end=ts_list[i+1],n=1,t=1440).process([]).__iter__():
             try:
                 if r[1]: pos+=1
             except IndexError: continue
             except TypeError: continue
             total+=1
+
+        if log:
+            print " ".join(['timestamp = ', str(ts_list[i]), ', total registrations = ',
+                            str(total), ', % prod editors = ',str(float(pos) / total)])
+
         yield (ts_list[i], total, float(pos) / total)
         # yields: (timestamp, total registrations, fraction of productive)
 
