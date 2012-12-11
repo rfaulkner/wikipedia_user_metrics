ad0f657737d4f58068a66d8d84c1e9044d8da2a2
rfaulk <rfaulkner@wikimedia.org>
2012-12-10 19:07:38 -0800

    ADD: Two stub files that will store implementations for logistic regression and support vector machine classifiers

---
+++ src/data_modelling/svn.py
@@ -0,0 +1,10 @@
+
+"""
+    Implements a support vector machine with LibSVM
+"""
+
+__author__ = "Ryan Faulkner <rfaulkner@wikimedia.org>"
+__date__ = "December 10th, 2012"
+__license__ = "GPL (version 2 or later)"
+
+class SVMClassifier(): raise NotImplementedError
