6057b3f4274aa37d81006ad9be63b03f8cc96b45
rfaulk <rfaulkner@wikimedia.org>
2012-12-12 13:01:46 -0800

    ADD: added aggregation method for revert metrics

--- src/etl/aggregator.py
+++ src/etl/aggregator.py
@@ -8,6 +8,7 @@ __date__ = "12/12/2012"
 __license__ = "GPL (version 2 or later)"
 
 import src.metrics.threshold as th
+import src.metrics.revert_rate as rr
 
 def decorator_builder(header):
     """ Decorator method to annotate aggregation methods to ensure the correct data model is exposed by """
@@ -34,6 +35,28 @@ def threshold_editors_agg(metric):
         except IndexError: continue
         except TypeError: continue
     return total, pos
+
+@decorator_builder(rr.RevertRate.header())
+def reverted_revs_agg(metric):
+    """ Computes revert metrics on a user set """
+    total_revs = 0
+    weighted_rate = 0.0
+    total_editors = 0
+    reverted_editors = 0
+    for r in metric.__iter__():
+        try:
+            reverted_revs = int(r[2])
+            total_editors += 1
+            if reverted_revs: reverted_editors += 1
+            total_revs += reverted_revs
+            weighted_rate += reverted_revs * float(r[1])
+        except IndexError: continue
+        except TypeError: continue
+    if total_revs:
+        weighted_rate /= total_revs
+    else:
+        weighted_rate = 0.0
+    return total_revs, weighted_rate, total_editors, reverted_editors
 
 class AggregatorException(Exception): pass
 
