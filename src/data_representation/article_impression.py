c054fd9eb352ab6663d878d4cbcd49610f1faf5d
rfaulk <rfaulkner@wikimedia.org>
2012-12-13 14:46:04 -0800

    ADD: stub files for factory classes to produce data representations asociated with Wikipedia articles and edits

---
+++ src/data_representation/article_impression.py
@@ -0,0 +1,9 @@
+"""
+    Factory for generating data points for Wikipedia articles.
+"""
+
+__author__ = "Ryan Faulkner <rfaulkner@wikimedia.org>"
+__date__ = "12/13/2012"
+__license__ = "GPL (version 2 or later)"
+
+class ArticleImpression(object): raise NotImplementedError
