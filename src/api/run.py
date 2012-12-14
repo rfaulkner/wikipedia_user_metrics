4fdbceaa410801c2f80a8c85590ec40ea57f5c6b
rfaulk <rfaulkner@wikimedia.org>
2012-12-13 19:03:20 -0800

    ADD: added some basic functionality to expose user tag meta data

--- src/api/run.py
+++ src/api/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-from flask import Flask, render_template # url_for, jsonify, 
+from flask import Flask, render_template, Markup # url_for, jsonify,
+import src.etl.data_loader as dl
 
 app = Flask(__name__)
 
@@ -13,7 +14,16 @@ def login():
     return render_template('login.html')
 
 @app.route('/tag_definitions')
-def tag_definitions(): pass
+def tag_definitions():
+
+    usertag_meta_data = ''
+    conn = dl.Connector(instance='slave')
+    conn._cur_.execute('select * from usertags_meta')
+
+    for r in conn._cur_:
+        usertag_meta_data += " ".join(dl.DataLoader().cast_elems_to_string(list(r))) + '<br>'
+
+    return render_template('tag_definitions.html', data=Markup(usertag_meta_data))
 
 @app.route('/cohorts')
 def api_cohorts():
