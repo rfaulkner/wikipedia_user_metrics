d9c57f21b9ea653f1d5d16b97d2f67ab0fb0308f
rfaulk <rfaulkner@wikimedia.org>
2012-12-17 12:44:44 -0800

    ADD: logger + simple data caching (pickled dictionary)
    https://github.com/rfaulkner/E3_analysis/issues/12

--- src/api/run.py
+++ src/api/run.py
@@ -7,12 +7,24 @@
 
 from flask import Flask, render_template, Markup, jsonify, redirect, url_for
 import src.etl.data_loader as dl
+import cPickle
+import logging
+import  sys
+import config.settings as settings
 
 import src.metrics.threshold as th
 # import src.metrics.blocks as b
 # import src.metrics.bytes_added as ba
 # import src.metrics.survival as sv
 # import src.metrics.time_to_threshold
+
+# CONFIGURE THE LOGGER
+logging.basicConfig(level=logging.DEBUG, stream=sys.stderr,
+    format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%b-%d %H:%M:%S')
+
+# Global instance of pickle file and results data
+pkl_file = None
+pkl_data = None
 
 app = Flask(__name__)
 
@@ -62,30 +74,54 @@ def metrics(cohort=''):
 @app.route('/metrics/<cohort>/<metric>')
 def output(cohort, metric):
 
-    # Get users
-    conn = dl.Connector(instance='slave')
-    try:
-        conn._cur_.execute('select utm_id from usertags_meta where utm_name = "%s"' % str(cohort))
-        res = conn._cur_.fetchone()[0]
-        conn._cur_.execute('select ut_user from usertags where ut_tag = "%s"' % res)
-    except IndexError:
-        redirect(url_for('cohorts'))
+    url = "".join(['/metrics/', cohort, '/', metric])
+    if url in pkl_data:
+        results = pkl_data[url]
+    else:
+        # Get users
+        conn = dl.Connector(instance='slave')
+        try:
+            conn._cur_.execute('select utm_id from usertags_meta where utm_name = "%s"' % str(cohort))
+            res = conn._cur_.fetchone()[0]
+            conn._cur_.execute('select ut_user from usertags where ut_tag = "%s"' % res)
+        except IndexError:
+            redirect(url_for('cohorts'))
 
-    # Get metric
-    metric_obj = get_metric(metric)
-    if not metric_obj: return redirect(url_for('cohorts'))
+        # Get metric
+        metric_obj = get_metric(metric)
+        if not metric_obj: return redirect(url_for('cohorts'))
 
-    results = dict()
-    results['header'] = " ".join(metric_obj.header())
-    results['metric'] = dict()
+        results = dict()
+        results['header'] = " ".join(metric_obj.header())
+        results['metric'] = dict()
 
-    users = [r[0] for r in conn._cur_]
+        users = [r[0] for r in conn._cur_]
 
-    for m in metric_obj.process(users, num_threads=20):
-        results['metric'][m[0]] = " ".join(dl.DataLoader().cast_elems_to_string(m[1:]))
+        for m in metric_obj.process(users, num_threads=20):
+            results['metric'][m[0]] = " ".join(dl.DataLoader().cast_elems_to_string(m[1:]))
 
-    del conn
-    return jsonify(results)
+        del conn
+        results = jsonify(results)
+        pkl_data[url] = results
+
+    return results
 
 if __name__ == '__main__':
-    app.run()
+
+    # Open the pickled data for reading.  Then
+    pkl_file = open(settings.__data_file_dir__ + 'api_data.pkl', 'rb')
+    pkl_data = cPickle.load(pkl_file)
+    pkl_file.close()
+
+    try:
+        app.run()
+    finally:
+        try:
+            pkl_file = open(settings.__data_file_dir__ + 'api_data.pkl', 'wb')
+            cPickle.dump(pkl_data, pkl_file)
+        except Exception:
+            logging.error('Could not pickle data.')
+            pass
+        pkl_file.close()
+
+
