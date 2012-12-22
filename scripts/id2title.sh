20817802c238dc833174edaa93729b3ddfec19c1
dartar <dario.taraborelli@gmail.com>
2012-12-21 16:38:03 -0800

    Wrong field name

--- scripts/id2title.sh
+++ scripts/id2title.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while read line
 do
-	c=$(mysql -B -e "SELECT page_id,page_name FROM enwiki.page WHERE page_id = \"$line\" LIMIT 1;" | sed '1d')
+	c=$(mysql -B -e "SELECT page_id,page_title FROM enwiki.page WHERE page_id = \"$line\" LIMIT 1;" | sed '1d')
 	echo $c
 done
