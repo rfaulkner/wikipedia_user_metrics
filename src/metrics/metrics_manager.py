ad237d1a8a9be3f8300c0237a443a90c165a9e4c
rfaulk <rfaulkner@wikimedia.org>
2012-12-31 12:41:41 -0800

    stub for file that manages handling metrics objects to produce aggregate data sets.

---
+++ src/metrics/metrics_manager.py
@@ -0,0 +1,8 @@
+
+"""
+    This module defines a set of methods useful in handling series of metrics objects to build more complex results.
+"""
+
+__author__ = "ryan faulkner"
+__date__ = "12/28/2012"
+__license__ = "GPL (version 2 or later)"
