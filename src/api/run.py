45310b7105c1741c53351f4c6be70d8965d324b5
rfaulk <rfaulkner@wikimedia.org>
2013-01-10 14:07:23 -0800

    Mod. Renamed strip_query_string() to process_request_params(). Docs and re-org of layout.

--- src/api/run.py
+++ src/api/run.py
@@ -29,6 +29,12 @@ import src.etl.data_loader as dl
 import src.metrics.metrics_manager as mm
 from src.api import COHORT_REGEX, parse_cohorts, MetricsAPIError
 
+######
+#
+# Define Globals and Data Types
+#
+#######
+
 app = Flask(__name__)
 
 # hash for jobs in queue_data dict
@@ -63,6 +69,13 @@ QUERY_VARIABLE_NAMES = collections.namedtuple('QVarNames', 'date_start date_end 
 
 DATETIME_STR_FORMAT = "%Y%m%d%H%M%S"
 
+
+######
+#
+# Define Decorators and helper methods
+#
+#######
+
 def get_errors(request_args):
     error = ''
     if 'error' in request_args:
@@ -72,165 +85,7 @@ def get_errors(request_args):
         except ValueError: pass
     return error
 
-@app.route('/')
-def api_root():
-    """ View for root url - API instructions """
-    conn = dl.Connector(instance='slave')
-    conn._cur_.execute('select utm_name from usertags_meta')
-    data = [r[0] for r in conn._cur_]
-    del conn
-
-    return render_template('index.html', cohort_data=data)
-
-@app.route('/tag_definitions')
-def tag_definitions():
-    """ View for tag definitions where cohort meta dat can be reviewed """
-
-    conn = dl.Connector(instance='slave')
-    conn._cur_.execute('select * from usertags_meta')
-
-    f = dl.DataLoader().cast_elems_to_string
-    usertag_meta_data = [escape(", ".join(f(r))) for r in conn._cur_]
-
-    del conn
-    return render_template('tag_definitions.html', data=usertag_meta_data)
-
-@app.route('/cohorts')
-def cohorts():
-    """ View for listing and selecting cohorts """
-
-    error = get_errors(request.args)
-
-    conn = dl.Connector(instance='slave')
-    conn._cur_.execute('select distinct utm_name from usertags_meta')
-    o = [r[0] for r in conn._cur_]
-
-    del conn
-    return render_template('cohorts.html', data=o, error=error)
-
-@app.route('/metrics')
-@app.route('/metrics/<string:cohort>')
-def metrics(cohort=''):
-    """ View for listing and selecting metrics """
-    if not cohort:
-        return redirect(url_for('cohorts'))
-    else:
-        return render_template('metrics.html', c_str=cohort, m_list=mm.get_metric_names())
-
-@app.route('/metrics/<string:cohort>/<string:metric>')
-def output(cohort, metric):
-    """ View corresponding to a data request - all of the setup and execution for a request happens here. """
-
-    global global_id
-    extra_params = list()   # stores extra query parameters that may be valid outside of metric parameters
-
-    url = request.url.split(request.url_root)[1]
-
-    # GET - parse query string
-    arg_dict = dict()
-    for key in request.args: arg_dict[key] = request.args[key]
-
-    refresh = False
-    if 'refresh' in arg_dict:
-        try:
-            if int(arg_dict['refresh']): refresh = True
-        except ValueError: pass
-
-    aggregator = arg_dict[QUERY_VARIABLE_NAMES.aggregator] if QUERY_VARIABLE_NAMES.aggregator in arg_dict else ''
-    aggregator_key = mm.get_agg_key(aggregator, metric)
-    if mm.aggregator_dict.has_key(aggregator_key):
-        extra_params.append(QUERY_VARIABLE_NAMES.aggregator)
-
-    if QUERY_VARIABLE_NAMES.time_series in arg_dict: extra_params.extend([
-        QUERY_VARIABLE_NAMES.time_series,
-        QUERY_VARIABLE_NAMES.date_start,
-        QUERY_VARIABLE_NAMES.date_end,
-        QUERY_VARIABLE_NAMES.interval])
-
-    # Format the query string
-    metric_params = mm.get_param_types(metric)
-
-    try:
-        url = strip_query_string(url, metric_params['init'].keys() + metric_params['process'].keys() + extra_params)
-    except MetricsAPIError as e:
-        return redirect(url_for('cohorts') + '?error=' + e.message)
-
-    if url in pkl_data and not refresh:
-        return pkl_data[url]
-    else:
-
-        # Ensure that the job for this url is not already running
-        is_pending_job = False
-        for p in processQ:
-            if not cmp(url, p.url) and p.status[0] == 'pending': is_pending_job = True
-
-        if not is_pending_job: # Queue the job
-
-            q = mp.Queue()
-            p = mp.Process(target=process_metrics, args=(url, cohort, metric, aggregator, q, arg_dict))
-            p.start()
-
-            global_id += 1
-
-            logging.info(__name__ + '::Appending request %s to the queue...' % url)
-            processQ.append(QStructClass(global_id,p,url,q,['pending']))
-
-            return render_template('processing.html', url_str=url)
-        else:
-            return redirect(url_for('job_queue') + '?error=0')
-
-@app.route('/job_queue')
-def job_queue():
-    """ View for listing current jobs working """
-
-    error = get_errors(request.args)
-
-    p_list = list()
-    p_list.append(Markup('<u><b>is_alive , PID, url, status</b></u><br>'))
-    for p in processQ:
-        try:
-
-            # Pull data off of the queue and add it to the queue data
-            while not p.queue.empty():
-                if not queue_data.has_key(p.id):
-                    queue_data[p.id] = json.loads(p.queue.get().data)
-                else:
-                    for k,v in queue_data[p.id]:
-                        if hasattr(v,'__iter__'): queue_data[p.id][k].extend(v)
-
-            # once a process has finished working remove it and put its contents into the cache
-            if not p.process.is_alive() and p.status[0] == 'pending':
-                q_response = make_response(jsonify(queue_data[p.id]))
-                del queue_data[p.id]
-                pkl_data[p.url] = q_response
-
-                p.status[0] = 'success'
-                logging.info(__name__ + '::Completed request %s.' % p.url)
-
-        except Exception as e:
-            p.status[0] = 'failure'
-            logging.error(__name__ + "::Could not update request: %s.  Exception: %s" % (p.url, e.message) )
-
-        # Log the status of the job
-        response_url = "".join(['<a href="', request.url_root, p.url + '">', p.url, '</a>'])
-        p_list.append(" , ".join([str(p.process.is_alive()), str(p.process.pid),
-                                  escape(Markup(response_url)), p.status[0]]))
-
-    if error:
-        return render_template('queue.html', procs=p_list, error=error)
-    else:
-        return render_template('queue.html', procs=p_list)
-
-@app.route('/all_urls')
-def all_urls():
-    """ View for listing all requests """
-
-    url_list = list()
-    for url in pkl_data.keys():
-        url_list.append("".join(['<a href="', request.url_root, url + '">', url, '</a>']))
-    return render_template('all_urls.html', urls=url_list)
-
-def strip_query_string(url, valid_items):
+def process_request_params(url, valid_items):
     """ Strips the query string down to the relevant items defined by the list of string objects `valid_items` """
 
     today = datetime.now()
@@ -315,6 +170,178 @@ def get_users(cohort_exp):
         del conn
     return users
 
+
+######
+#
+# Define View methods
+#
+#######
+
+@app.route('/')
+def api_root():
+    """ View for root url - API instructions """
+    conn = dl.Connector(instance='slave')
+    conn._cur_.execute('select utm_name from usertags_meta')
+    data = [r[0] for r in conn._cur_]
+    del conn
+
+    return render_template('index.html', cohort_data=data)
+
+@app.route('/tag_definitions')
+def tag_definitions():
+    """ View for tag definitions where cohort meta dat can be reviewed """
+
+    conn = dl.Connector(instance='slave')
+    conn._cur_.execute('select * from usertags_meta')
+
+    f = dl.DataLoader().cast_elems_to_string
+    usertag_meta_data = [escape(", ".join(f(r))) for r in conn._cur_]
+
+    del conn
+    return render_template('tag_definitions.html', data=usertag_meta_data)
+
+@app.route('/cohorts')
+def cohorts():
+    """ View for listing and selecting cohorts """
+
+    error = get_errors(request.args)
+
+    conn = dl.Connector(instance='slave')
+    conn._cur_.execute('select distinct utm_name from usertags_meta')
+    o = [r[0] for r in conn._cur_]
+
+    del conn
+    return render_template('cohorts.html', data=o, error=error)
+
+@app.route('/metrics')
+@app.route('/metrics/<string:cohort>')
+def metrics(cohort=''):
+    """ View for listing and selecting metrics """
+    if not cohort:
+        return redirect(url_for('cohorts'))
+    else:
+        return render_template('metrics.html', c_str=cohort, m_list=mm.get_metric_names())
+
+@app.route('/metrics/<string:cohort>/<string:metric>')
+def output(cohort, metric):
+    """ View corresponding to a data request - all of the setup and execution for a request happens here. """
+
+    global global_id
+    extra_params = list()   # stores extra query parameters that may be valid outside of metric parameters
+
+    url = request.url.split(request.url_root)[1]
+
+    # GET - parse query string
+    arg_dict = dict()
+    for key in request.args: arg_dict[key] = request.args[key]
+
+    refresh = False
+    if 'refresh' in arg_dict:
+        try:
+            if int(arg_dict['refresh']): refresh = True
+        except ValueError: pass
+
+    aggregator = arg_dict[QUERY_VARIABLE_NAMES.aggregator] if QUERY_VARIABLE_NAMES.aggregator in arg_dict else ''
+    aggregator_key = mm.get_agg_key(aggregator, metric)
+    if mm.aggregator_dict.has_key(aggregator_key):
+        extra_params.append(QUERY_VARIABLE_NAMES.aggregator)
+
+    if QUERY_VARIABLE_NAMES.time_series in arg_dict: extra_params.extend([
+        QUERY_VARIABLE_NAMES.time_series,
+        QUERY_VARIABLE_NAMES.date_start,
+        QUERY_VARIABLE_NAMES.date_end,
+        QUERY_VARIABLE_NAMES.interval])
+
+    # Format the query string
+    metric_params = mm.get_param_types(metric)
+
+    try:
+        url = process_request_params(url, metric_params['init'].keys() + metric_params['process'].keys() + extra_params)
+    except MetricsAPIError as e:
+        return redirect(url_for('cohorts') + '?error=' + e.message)
+
+    if url in pkl_data and not refresh:
+        return pkl_data[url]
+    else:
+
+        # Ensure that the job for this url is not already running
+        is_pending_job = False
+        for p in processQ:
+            if not cmp(url, p.url) and p.status[0] == 'pending': is_pending_job = True
+
+        if not is_pending_job: # Queue the job
+
+            q = mp.Queue()
+            p = mp.Process(target=process_metrics, args=(url, cohort, metric, aggregator, q, arg_dict))
+            p.start()
+
+            global_id += 1
+
+            logging.info(__name__ + '::Appending request %s to the queue...' % url)
+            processQ.append(QStructClass(global_id,p,url,q,['pending']))
+
+            return render_template('processing.html', url_str=url)
+        else:
+            return redirect(url_for('job_queue') + '?error=0')
+
+@app.route('/job_queue')
+def job_queue():
+    """ View for listing current jobs working """
+
+    error = get_errors(request.args)
+
+    p_list = list()
+    p_list.append(Markup('<u><b>is_alive , PID, url, status</b></u><br>'))
+    for p in processQ:
+        try:
+
+            # Pull data off of the queue and add it to the queue data
+            while not p.queue.empty():
+                if not queue_data.has_key(p.id):
+                    queue_data[p.id] = json.loads(p.queue.get().data)
+                else:
+                    for k,v in queue_data[p.id]:
+                        if hasattr(v,'__iter__'): queue_data[p.id][k].extend(v)
+
+            # once a process has finished working remove it and put its contents into the cache
+            if not p.process.is_alive() and p.status[0] == 'pending':
+                q_response = make_response(jsonify(queue_data[p.id]))
+                del queue_data[p.id]
+                pkl_data[p.url] = q_response
+
+                p.status[0] = 'success'
+                logging.info(__name__ + '::Completed request %s.' % p.url)
+
+        except Exception as e:
+            p.status[0] = 'failure'
+            logging.error(__name__ + "::Could not update request: %s.  Exception: %s" % (p.url, e.message) )
+
+        # Log the status of the job
+        response_url = "".join(['<a href="', request.url_root, p.url + '">', p.url, '</a>'])
+        p_list.append(" , ".join([str(p.process.is_alive()), str(p.process.pid),
+                                  escape(Markup(response_url)), p.status[0]]))
+
+    if error:
+        return render_template('queue.html', procs=p_list, error=error)
+    else:
+        return render_template('queue.html', procs=p_list)
+
+@app.route('/all_urls')
+def all_urls():
+    """ View for listing all requests """
+
+    url_list = list()
+    for url in pkl_data.keys():
+        url_list.append("".join(['<a href="', request.url_root, url + '">', url, '</a>']))
+    return render_template('all_urls.html', urls=url_list)
+
+
+######
+#
+# Define Custom Classes
+#
+#######
+
 class APIMethods(object):
     """ Provides initialization and boilerplate for API execution """
 
@@ -353,6 +380,13 @@ class APIMethods(object):
             logging.error(__name__ + '::Could not pickle data.')
         finally:
             if hasattr(pkl_file, 'close'): pkl_file.close()
+
+
+######
+#
+# Execution
+#
+#######
 
 if __name__ == '__main__':
 
