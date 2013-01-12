55cff622e0b0a8f522ff1ac82d3e619657cd4204
dartar <dario.taraborelli@gmail.com>
2013-01-11 22:26:29 -0800

    Sample links

--- src/api/run.py
+++ src/api/run.py
@@ -66,7 +66,8 @@
         +-------------+-----------------+------+-----+---------+----------------+
 
 """
-
+import sys
+sys.path.insert(0,"/Users/dartar/git/E3_analysis")
 from flask import Flask, render_template, Markup, jsonify, \
     redirect, url_for, make_response, request, escape
 
