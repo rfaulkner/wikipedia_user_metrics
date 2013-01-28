f38863215d8a41385e29e5ccc28cce98cf9e2d58
rfaulk <rfaulkner@wikimedia.org>
2013-01-28 11:56:05 -0800

    Rm. removed specialized time_series methods.

--- src/etl/time_series_process_methods.py
+++ src/etl/time_series_process_methods.py
@@ -17,9 +17,7 @@ import logging
 import operator
 import json
 
-import src.metrics.threshold as th
 import src.metrics.revert_rate as rr
-import src.etl.data_loader as dl
 import src.metrics.user_metric as um
 from multiprocessing import Process, Queue
 
@@ -47,98 +45,6 @@ def _get_timeseries(date_start, date_end, interval):
     while c < e:
         c += datetime.timedelta(hours=interval)
         yield c
-
-def _get_newly_registered_users(date_start, date_end, project):
-    """ Produces a set of newly registered users givem """
-    sql = """
-            SELECT
-                log_user
-            FROM %(project)s.logging
-            WHERE log_timestamp >= %(date_start)s AND log_timestamp <=
-                %(date_end)s AND log_action = 'create' AND
-                log_type='newusers'
-        """ % {
-        'project' : project,
-        'date_start' : date_start,
-        'date_end' : date_end
-    }
-    return dl.DataLoader().get_elem_from_nested_list(
-        dl.Connector(instance='slave').execute_SQL(" ".join(sql.strip().split(
-            '\n'))),0)
-
-def threshold_editors(args,interval,log=True,project='enwiki',k=1,n=1,t=1440):
-    """ Computes a list of threshold metrics """
-
-    time_series = _get_timeseries(args.date_start, args.date_end, interval)
-
-    ts_s = time_series.next()
-    while 1:
-        try:
-            ts_e = time_series.next()
-        except StopIteration:
-            break
-
-        if log: print str(datetime.datetime.now()) + \
-                      ' - Processing time series data for %s...' % str(ts_s)
-        user_ids = _get_newly_registered_users(args.date_start,
-            args.date_end,project)
-
-        # Build an iterator across users for a given threshold
-        threshold_obj = th.Threshold(date_start=ts_s, date_end=ts_e,n=n,t=t).\
-        process(user_ids, log_progress=True, num_threads=k)
-        total, pos, rate = th.threshold_editors_agg(threshold_obj)
-
-        if log: print " ".join(['timestamp = ',
-                                str(ts_s),
-                                ', total registrations = ',
-                                str(total),
-                                ', % prod editors = ',
-                                str(rate)])
-
-        # yields: (timestamp, total registrations, fraction of productive)
-        yield (ts_s, total, rate)
-        ts_s = ts_e
-
-def reverted(args,interval,log=True,project='enwiki',la=15,lb=15,k=1):
-
-    """ Computes a list of threshold metrics """
-
-    time_series = _get_timeseries(args.date_start, args.date_end, interval)
-    ts_s = time_series.next()
-
-    while 1:
-        try:
-            ts_e = time_series.next()
-        except StopIteration:
-            break
-
-        if log: print str(datetime.datetime.now()) + \
-                      ' - Processing time series data for %s...' % str(ts_s)
-        user_ids = _get_newly_registered_users(args.date_start,
-            args.date_end,project)
-
-        revert_obj = rr.RevertRate(date_start=ts_s, date_end=ts_e,
-            look_ahead=la,look_back=lb).process(
-            user_ids, log_progress=True, num_threads=k)
-        total_revs, weighted_rate, total_editors, reverted_editors = \
-            rr.reverted_revs_agg(revert_obj)
-
-        if log:
-            logging.info(__name__ +
-                         " ".join(['timestamp = ',
-                            str(ts_s),
-                            ', total revisions = ',
-                            str(total_revs),
-                            ', total revert rate = ',
-                            str(weighted_rate),
-                            ' total editors = ',
-                            str(total_editors),
-                            ' reverted editors = ',
-                            str(reverted_editors)]))
-
-        yield (ts_s, total_revs, weighted_rate, total_editors,
-               reverted_editors)
-        ts_s = ts_e
 
 def build_time_series(start, end, interval, metric, aggregator, cohort,
                       **kwargs):
@@ -254,9 +160,6 @@ def time_series_worker(time_series, metric, aggregator, cohort, kwargs, q):
 
         ts_s = ts_e
     q.put(data) # add the data to the queue
-
-class DataTypeMethods(object):
-    DATA_TYPE = {'prod' : threshold_editors, 'revert' : reverted}
 
 class TimeSeriesException(Exception):
     """ Basic exception class for UserMetric types """
