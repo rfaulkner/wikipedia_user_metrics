816f88df1f2fe6b6d6a6ac428d0722c20565bbbf
rfaulk <rfaulkner@wikimedia.org>
2013-01-30 17:16:27 -0800

    mod. update setup definition from distutils.

--- setup.py
+++ setup.py
@@ -6,25 +6,20 @@ from distutils.core import setup
 with open('README.md') as file:
     long_description = file.read()
 
+__version__ = '0.1-dev'
+
 setup(
     name='e3_analysis',
-    version='0.1-dev',
-    email="e3-team@lists.wikimedia.org",
+    version=__version__,
     long_description=long_description,
     description='Data Analysis for Wikipedia User data.',
     url='http://www.github.com/rfaulkner/E3_analysis',
-    author='Ryan Faulkner',
-    author_email='rfaulkner@wikimedia.org',
-    long_description=__doc__,
-    packages=['e3_analysis.src', 'e3_analysis.src.etl',
-              'e3_analysis.src.metrics','e3_analysis.config'],
-    scripts=['e3_analysis/scripts/e3_data_wrangle',
-             'e3_analysis/scripts/e3_experiment_definitions.py',
-             'e3_analysis/scripts/call_metric_on_users',
-             'e3_analysis/scripts/daily_active_users',
-             'e3_analysis/scripts/id2user.sh',
-             'e3_analysis/scripts/user2id.sh',
-             'e3_analysis/scripts/e3_settings.py'],
+    author="Wikimedia Foundation",
+    author_email="e3-team@lists.wikimedia.org",
+    packages=['e3_analysis.src',
+              'e3_analysis.src.etl',
+              'e3_analysis.src.metrics',
+              'e3_analysis.config'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
