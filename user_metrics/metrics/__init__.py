5445231bd2011532866046a15643715b69602da4
rfaulk <rfaulkner@wikimedia.org>
2013-03-08 13:44:08 -0800

    mod. import in metrics init of query module no longer relies on sys.path

--- user_metrics/metrics/__init__.py
+++ user_metrics/metrics/__init__.py
@@ -2,5 +2,4 @@
 # Module path initialization - define the query module based on config
 
 from user_metrics.config import settings
-QUERY_MOD_PATH = 'user_metrics.query.'
-query_mod = __import__(QUERY_MOD_PATH + settings.__query_module__)
+query_mod = __import__(settings.__query_module__)
