177b7e8684b1d1176c22cf400089aa4fc119c3ef
rfaulk <rfaulkner@wikimedia.org>
2013-03-08 13:31:31 -0800

    mod. import statement to use package definition.

--- user_metrics/metrics/__init__.py
+++ user_metrics/metrics/__init__.py
@@ -1,9 +1,6 @@
 
 # Module path initialization - define the query module based on config
 
-import sys
 from user_metrics.config import settings
-QUERY_MOD_PATH = 'user_metrics/query/'
-sys.path.append(settings.__project_home__ + QUERY_MOD_PATH)
-
-query_mod = __import__(settings.__query_module__)
+QUERY_MOD_PATH = 'user_metrics.query.'
+query_mod = __import__(QUERY_MOD_PATH + settings.__query_module__)
