a7c5db0eca852697d295c74012e9a9f753ea7e3f
rfaulk <rfaulkner@wikimedia.org>
2013-03-12 02:20:09 -0700

    add. initial user authentication hooks via flask-login extension.

--- user_metrics/api/views.py
+++ user_metrics/api/views.py
@@ -17,7 +17,7 @@ __license__ = "GPL (version 2 or later)"
 
 
 from flask import Flask, render_template, Markup, redirect, url_for, \
-    request, escape, jsonify, make_response
+    request, escape, jsonify, make_response, flash
 from re import search, sub
 from collections import OrderedDict
 
@@ -296,3 +296,70 @@ def process_responses():
 
         data = make_response(jsonify(res[1]))
         set_data(request_meta, data, api_data)
+
+
+# User Authentication
+# ###################
+
+from flask.ext.login import (LoginManager, current_user, login_required,
+                             login_user, logout_user, UserMixin, AnonymousUser,
+                             confirm_login, fresh_login_required)
+
+
+class User(UserMixin):
+    def __init__(self, name, id, active=True):
+        self.name = name
+        self.id = id
+        self.active = active
+
+    def is_active(self):
+        return self.active
+
+
+class Anonymous(AnonymousUser):
+    name = u'Anonymous'
+
+
+login_manager = LoginManager()
+
+login_manager.anonymous_user = Anonymous
+login_manager.login_view = 'login'
+login_manager.login_message = u'Please log in to access this page.'
+login_manager.refresh_view = 'reauth'
+
+USERS = {
+    1: User(u"Notch", 1),
+    2: User(u"Steve", 2),
+    3: User(u"Creeper", 3, False),
+    }
+
+USER_NAMES = dict((u.name, u) for u in USERS.itervalues())
+
+
+@login_manager.user_loader
+def load_user(uid):
+    return USERS.get(int(uid))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'username' in request.form:
+        username = request.form['username']
+        if username in USER_NAMES:
+            remember = request.form.get('remember', 'no') == 'yes'
+            if login_user(USER_NAMES[username], remember=remember):
+                flash('Logged in!')
+                return redirect(request.args.get('next') or url_for('api_root'))
+            else:
+                flash('Sorry, but you could not log in.')
+        else:
+            flash(u"Invalid username.")
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out.')
+    return redirect(url_for('api_root'))
