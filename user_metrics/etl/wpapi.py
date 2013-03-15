42292fcda1b4a1a9b305bf3c09f2856607bf841e
rfaulk <rfaulkner@wikimedia.org>
2013-03-15 13:31:29 -0700

    mod. import logger, pep8.

--- user_metrics/etl/wpapi.py
+++ user_metrics/etl/wpapi.py
@@ -1,12 +1,15 @@
 """
-    This module defines a class for interfacing with http://www.mediawiki.org/wiki/API:Main_page
+    This module defines a class for interfacing with
+        http://www.mediawiki.org/wiki/API:Main_page
 
     Example: ::
 
         >>> import libraries.etl.WPAPI
         >>> api = WPAPI.WPAPI()
         >>> api.getDiff(515866670)
-        (u'[[Category:People from Palermo]] [[Category:Sportspeople from Sicily|Palermo]] [[Category:Sport in Palermo|People]] [[Category:Sportspeople by city in Italy|Palermo]]', True    )
+        (u'[[Category:People from Palermo]] [[Category:Sportspeople from
+        Sicily|Palermo]] [[Category:Sport in Palermo|People]] [[Category:
+        Sportspeople by city in Italy|Palermo]]', True    )
 """
 
 __author__ = "Ryan Faulkner and Aaron Halfaker"
@@ -14,7 +17,6 @@ __date__ = "October 3rd, 2012"
 __license__ = "GPL (version 2 or later)"
 
 import sys
-import logging
 import types
 import re
 import time
@@ -22,18 +24,18 @@ import urllib
 import urllib2
 import json
 import htmlentitydefs
+from user_metrics.config import logging
 
-# CONFIGURE THE LOGGER
-logging.basicConfig(level=logging.DEBUG, stream=sys.stderr, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%b-%d %H:%M:%S')
 
 class WPAPI:
     """
-        The class itself implements functionality that allows a user to examine revision text.
-
-        The constructor allows the user ot specify the particular api.
+        The class itself implements functionality that allows a user to
+        examine revision text.  The initializerallows the user ot specify
+        the particular API.
     """
 
-    DIFF_ADD_RE = re.compile(r'<td class="diff-addedline"><div>(.+)</div></td>')
+    DIFF_ADD_RE = re.compile(
+        r'<td class="diff-addedline"><div>(.+)</div></td>')
 
     def __init__(self, uri='http://en.wikipedia.org/w/api.php'):
         self.uri = uri
@@ -44,7 +46,9 @@ class WPAPI:
 
         while attempt < retries:
             try:
-                # e.g. url: http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&revids=472419240&rvprop=ids&rvdiffto=prev&format=json
+                # e.g. url: http://en.wikipedia.org/w/api.php?format=xml&
+                # action=query&prop=revisions&revids=472419240&rvprop=ids&
+                # rvdiffto=prev&format=json
                 response = urllib2.urlopen(
                     self.uri,
                     urllib.urlencode({
@@ -59,13 +63,17 @@ class WPAPI:
 
                 result = json.load(response)
 
-                diff = result['query']['pages'].values()[0]['revisions'][0]['diff']['*']
+                diff = result['query']['pages'].values()[0][
+                    'revisions'][0]['diff']['*']
 
-                # The diff will not exist if it included the creation of the user talk page
-                # in this case simply load the content of the page at this revision
+                # The diff will not exist if it included the creation of the
+                # user talk page in this case simply load the content of the
+                # page at this revision
                 if type(diff) not in types.StringTypes or diff == '':
 
-                    # e.g. url: http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&revids=474338555&format=json&rvprop=content
+                    # e.g. url: http://en.wikipedia.org/w/api.php?format=xml&
+                    # action=query&prop=revisions&revids=474338555&format=json&
+                    # rvprop=content
                     response = urllib2.urlopen(
                         self.uri,
                         urllib.urlencode({
@@ -79,14 +87,17 @@ class WPAPI:
 
                     result = json.load(response)
                     try:
-                        diff = result['query']['pages'].values()[0]['revisions'][0]['*']
+                        diff = result['query']['pages'].\
+                            values()[0]['revisions'][0]['*']
                     except KeyError:
                         sys.stderr.write("x")
                         diff = ''
                         pass
 
-                    # Add the diff tags such that the content is parsed as if it were a diff
-                    if type(diff) not in types.StringTypes: diff = ''
+                    # Add the diff tags such that the content is parsed as if
+                    # it were a diff
+                    if type(diff) not in types.StringTypes:
+                        diff = ''
 
                     is_content = True
 
@@ -94,8 +105,8 @@ class WPAPI:
             except urllib2.HTTPError as e:
                 time.sleep(2**attempt)
                 attempt += 1
-                logging.error("HTTP Error: %s.  Retry #%s in %s seconds..." % (e, attempt, 2**attempt))
-
+                logging.error("HTTP Error: %s.  Retry #%s in %s seconds..." % (
+                    e, attempt, 2**attempt))
 
     def getAdded(self, revId):
         diff, is_content = self.getDiff(revId)
@@ -106,7 +117,7 @@ class WPAPI:
             return self.unescape(
                 "\n".join(
                     match.group(1)
-                        for match in WPAPI.DIFF_ADD_RE.finditer(diff)
+                    for match in WPAPI.DIFF_ADD_RE.finditer(diff)
                 )
             )
 
@@ -128,5 +139,6 @@ class WPAPI:
                     text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
                 except KeyError:
                     pass
-            return text # leave as is
+            # leave as is
+            return text
         return re.sub("&#?\w+;", fixup, text)
