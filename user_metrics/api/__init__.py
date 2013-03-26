5da156a6f0d08ee4952711e00430aa9803de169c
rfaulk <rfaulkner@wikimedia.org>
2013-03-26 16:58:54 -0700

    add. error_code property to MetricsAPIError exception class.  Move defnition of error codes into __init__.

--- user_metrics/api/__init__.py
+++ user_metrics/api/__init__.py
@@ -5,8 +5,26 @@
     are defined at https://meta.wikimedia.org/wiki/Research:Metrics.
 """
 
+# Error codes for web requests
+# ############################
+
+error_codes = {
+    -1: 'Metrics API HTTP request error.',
+    0: 'Job already running.',
+    1: 'Badly Formatted timestamp',
+    2: 'Could not locate stored request.',
+    3: 'Could not find User ID.',
+    4: 'Bad metric name.',
+}
+
 
 class MetricsAPIError(Exception):
     """ Basic exception class for UserMetric types """
-    def __init__(self, message="Error processing API request."):
+    def __init__(self, message="Error processing API request.",
+                 error_code=-1):
+        self.error_code_index = error_code
         Exception.__init__(self, message)
+
+    @property
+    def error_code(self):
+        return self.error_code_index
